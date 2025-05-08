981cc70f79a5742949ca6cd19c9836b23c9ce8fd
iiPython <ben@iipython.dev>
2025-05-08 16:38:03 -0500

    another save

--- src/web/js/html.js
+++ src/web/js/html.js
@@ -1,35 +1,105 @@
 // Copyright (c) 2025 iiPython
 
-const main = document.querySelector("main");
-const display = (id) => {
-    switch (id) {
-        case "no_drive":
-            main.innerHTML = `
-                <section id = "error">
-                    <h2>No Optical Drive</h2>
-                    <p>The system did not return a valid optical drive, make sure you have one connected.</p>
-                </section>
-            `;
-            break;
+import { fetch_cover } from "./api.js";
 
-        case "no_disc":
-            main.innerHTML = `
-                <section id = "error">
-                    <h2>Disc Required</h2>
-                    <p>In order to rip a CD, the disc must be inserted into your optical drive. This program can not break the laws of physics.</p>
-                </section>
-            `;
-            break;
+// Handle templating
+function generate_error(header, description) {
+    return `
+        <section id = "error">
+            <h2>${header}</h2>
+            <p>${description}</p>
+        </section>
+    `;
+}
 
-        case "no_match":
-            main.innerHTML = `
-                <section id = "error">
-                    <h2>MusicBrainz Lookup Failed</h2>
-                    <p>The CD in your optical drive was not found in the Musicbrainz database. Ensure the disc is free of scratches so the TOC can be pulled successfully.</p>
+const convert_time = (milliseconds) => {
+    let seconds = milliseconds / 1000;
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    minutes -= hours * 60;
+    seconds = Math.round(seconds - (60 * minutes)).toString();
+    return `${hours ? hours.toString().padStart(2, "0") + ":" : ""}${minutes.toString().padStart(2, "0")}:${seconds.padStart(2, "0")}`;
+}
+
+// Setup actual templates
+const templates = {
+    "no-drive": () => {
+        document.querySelector("main").innerHTML = generate_error(
+            "No Optical Drive",
+            "The system did not return a valid optical drive, make sure you have one connected."
+        );
+    },
+    "no-disc": () => {
+        document.querySelector("main").innerHTML = generate_error(
+            "Disc Required",
+            "In order to rip a CD, the disc must be inserted into your optical drive. This program can not break the laws of physics."
+        );
+    },
+    "no-match": () => {
+        document.querySelector("main").innerHTML = generate_error(
+            "Musicbrainz Lookup Failed",
+            "The CD in your optical drive was not found in the Musicbrainz database. Ensure the disc is free of scratches so the TOC can be pulled successfully."
+        );
+    },
+    "rip-ui": async (data) => {
+        const { disc, drive } = data;
+
+        // Process data
+        const artist_name = disc["artist-credit"][0].name;
+        document.querySelector("main").innerHTML = `
+            <header>
+                <section>
+                    <img id = "cover" src = "${fetch_cover(disc.id)}">
                 </section>
+                <section id = "title">
+                    <h1>${disc.title}</h1>
+                    <h2>${artist_name}</h2>
+                </section>
+            </header>
+            <footer>
+                <section id = "tracks">
+                    <div class = "track-container">
+                        <table>
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Length</th>
+                                <th></th>
+                            </tr>
+                        </table>
+                    </div>
+                    <button id = "rip">Start ripping</button>
+                    <div id = "progress"></div>
+                </section>
+                <section id = "metadata">
+                    <span>Date: ${disc.date}</span>
+                    <span>Runtime: ${convert_time(disc.media[0].tracks.map(track => track.length).reduce((x, y) => x + y))}</span>
+                    <span>Country: ${disc.country}</span>
+                    <span>Drive: ${drive}</span>
+                    <hr>
+                    <a target = "_blank" href = "https://musicbrainz.org/release/${disc.id}">MusicBrainz</a>
+                    <a target = "_blank" href = "https://last.fm/music/${artist_name}/${disc.title}">Last.fm</a>
+                    <hr>
+                    <div>
+                        <input type = "checkbox" id = "open-in-picard">
+                        <label for = "open-in-picard">Open in Picard</label>
+                    </div>
+                </section>
+            </footer>
+        `;
+
+        // Load in tracks
+        for (const track of disc.media[0].tracks) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${}</td>
+                <td>${}</td>
+                <td>${}</td>
+                <td>${}</td>
             `;
-            break;
+            document.querySelector("table").appendChild(row);
+        }
     }
 }
 
-export { main, display };
+export { templates };
--- src/web/js/renderer.js
+++ src/web/js/renderer.js
@@ -1,201 +1,28 @@
 // Copyright (c) 2025 iiPython
 
-// Modules
-import { main, display } from "./html.js";
+import Connection from "./connection.js";
+import { templates } from "./html.js";
+import { lookup_disc } from "./api.js";
 
-// Handle metadata
-const musicbrainz = async (endpoint) => {
-    return await (
-        await fetch(`https://musicbrainz.org/ws/2/${endpoint}`, {
-            headers: { "User-Agent": "iiPython CD Ripper/0.3.1 (https://github.com/iiPythonx/cdrip; ben@iipython.dev)" }
-        })
-    ).json();
-}
+new class {
+    #connection;
+    #disc;
 
-// Main class
-new (class {
     constructor() {
-        this.websocket = new WebSocket("ws://localhost:8000/ws");
-        this.websocket.addEventListener("open", () => {
-            this.websocket.addEventListener("message", (e) => {
-                const data = JSON.parse(e.data);
-                console.log(data);
-                switch (data.type) {
-                    case "update":
-                        this.on_update(data.progress);
-                        break;
-
-                    case "flac-complete":
-                        this.on_update("done");
-                        this.resolve();
-                        break;
-
-                    default:
-                        if (this.resolve) {
-                            this.resolve(data.value);
-                            delete this.resolve;
-                        }
-                        break;
-                }
-            });
-
-            this.check_interval = setInterval(() => this.check_drive(), 1000);
-            this.check_drive();
-        });
-
+        this.#connection = new Connection();
+        this.#connection.bind("status-changed", (result) => this.#status_changed(result));
     }
 
-    send(type, ...args) {
-        return new Promise((resolve) => {
-            this.resolve = resolve;
-            this.websocket.send(JSON.stringify({ type, args }));
-        });
-    }
+    async #status_changed(result) {
+        console.log("[Status Change]", result);
+        if (result.status !== "has-disc") return templates[result.status]();
 
-    async show_disc() {
-        this.disc.artist = this.disc["artist-credit"][0]["name"];
-        
-        const convert_time = (milliseconds) => {
-            let seconds = milliseconds / 1000;
-            let minutes = Math.floor(seconds / 60);
-            let hours = Math.floor(minutes / 60);
-            minutes -= hours * 60;
-            seconds = Math.round(seconds - (60 * minutes)).toString();
-            return `${hours ? hours.toString().padStart(2, "0") + ":" : ""}${minutes.toString().padStart(2, "0")}:${seconds.padStart(2, "0")}`;
-        }
+        // Lookup disc
+        this.#disc = await lookup_disc(result.data.toc);
+        if (!this.#disc) return templates["no-match"]();
+        console.log(this.#disc);
 
-        main.innerHTML = `
-            <header>
-                <section>
-                    <img id = "cover" src = "http://coverartarchive.org/release/${this.disc.id}/front-250">
-                </section>
-                <section id = "title">
-                    <h1>${this.disc.title}</h1>
-                    <h2>${this.disc.artist}</h2>
-                </section>
-            </header>
-            <footer>
-                <section id = "tracks">
-                    <div class = "track-container">
-                        <table>
-                            <tr>
-                                <th>#</th>
-                                <th>Title</th>
-                                <th>Length</th>
-                                <th></th>
-                            </tr>
-                        </table>
-                    </div>
-                    <button id = "rip">Start ripping</button>
-                    <div id = "progress"></div>
-                </section>
-                <section id = "metadata">
-                    <span>Date: ${this.disc.date}</span>
-                    <span>Runtime: ${convert_time(this.disc.media[0].tracks.map(track => track.length).reduce((x, y) => x + y))}</span>
-                    <span>Country: ${this.disc["release-events"][0].area["iso-3166-1-codes"][0]}</span>
-                    <span>Drive: ${this.drive}</span>
-                    <hr>
-                    <a href = "https://musicbrainz.org/release/${this.disc.id}" target = "_blank">MusicBrainz</a>
-                    <a href = "https://last.fm/music/${this.disc.artist}/${this.disc.title}" target = "_blank">Last.fm</a>
-                    <hr>
-                    <div>
-                        <input type = "checkbox" id = "open-in-picard">
-                        <label for = "open-in-picard">Open in Picard</label>
-                    </div>
-                </section>
-            </footer>
-        `;
-
-        // Populate track list
-        for (const track of this.disc.media[0].tracks) {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${track.number}</td>
-                <td>${track.title}</td>
-                <td>${convert_time(track.length)}</td>
-                <td data-track-number = "${track.number}"></td>
-            `;
-            document.querySelector("tbody").appendChild(row);
-        }
-
-        // Ripping phase
-        document.querySelector("#rip").addEventListener("click", async (e) => {
-            if (e.currentTarget.innerText === "Eject disc") {
-                this.send("eject_disc");
-                this.check_interval = setInterval(() => this.check_drive(), 1000);
-                this.check_drive();
-                return;
-            }
-            this.rip_disc();
-        });
-    }
-
-    async on_update(update) {
-        const track_count = this.disc.media[0].tracks.length;
-        if (update === "done") {
-            document.querySelector("button").innerText = "Eject disc";
-            document.querySelector("button").disabled = false;
-            return;
-        }
-        document.querySelector("#progress").style.width = `${(update / (track_count * 2)) * 100}%`;
-        if (update <= track_count) document.querySelector(`td[data-track-number = "${+update}"]`).innerText = "✓";
-    }
-
-    async rip_disc() {
-        const path = await new Promise(async (resolve) => {
-            const dialog = document.createElement("dialog");
-            dialog.open = true;
-            dialog.innerHTML = `
-                <div>
-                    <h2>Select storage path</h2>
-                    <input value = "${await this.send("fetch_music_path")}" id = "path" autofocus>
-                    <button id = "path-confirm">Rip!</button>
-                </div>
-            `;
-            document.body.appendChild(dialog);
-            document.querySelector("#path-confirm").addEventListener("click", () => {
-                resolve(document.querySelector("#path").value);
-                dialog.remove();
-            })
-        });
-
-        // Calculate album path
-        const folder_path = `${path}/${this.disc.artist} - ${this.disc.title}/`;
-
-        document.querySelector("button").innerText = "Ripping...";
-        document.querySelector("button").disabled = true;
-        await this.send("rip_disc", folder_path);  // do NOT remove end slash
-        await this.send(
-            "convert_flac",
-            folder_path,
-            this.disc.media[0].tracks.map(track => [
-                `${track.number.padStart(2, "0")} ${track.title.replace("/", "_")}.flac`,
-                track.title
-            ]),
-            this.disc.id
-        );
-        if (document.querySelector("#open-in-picard").checked) this.send("open_picard", folder_path);
-    }
-
-    async fetch_metadata(toc) {
-        const response = await musicbrainz(`discid/-?toc=${toc}&fmt=json`);
-        if (response.error || !response.releases || !response.releases.length) return null;
-        return await musicbrainz(`release/${response["releases"][0].id}?inc=artists+recordings&fmt=json`);
-    }
-
-    async check_drive() {
-        this.drive = await this.send("fetch_drive");
-        if (!this.drive) return display("no_drive");
-        
-        const toc = await this.send("fetch_toc");
-        if (!toc) return display("no_disc");
-        
-        clearInterval(this.check_interval);
-        
-        this.toc = toc;
-        this.disc = await this.fetch_metadata(toc);
-
-        if (!this.disc) return display("no_match");
-        await this.show_disc();
+        // Show ripping interface
+        templates["rip-ui"]({ disc: this.#disc, drive: result.data.drive });
     }
 });
